fe06dbf19a64930264a1c72a1b86d4b90965f40f
DESKTOP-CF00VG9\User <paingthumyo41297@gmail.com>
2021-12-15 10:00:19 +0630

    [master] finished php tutorial 1

---
+++ tutorials/tutorial_01/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Top | Tutorial 1</title>
+  <link rel="stylesheet" href="css/reset.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+  <h1>Tutorial 1 Assignment</h1>
+  <table>
+    <?php
+      for ($row = 0; $row < 8; $row++) {
+          echo "<tr>";
+          $row_count = $row;
+
+          for ($i = 0; $i < 8; $i++) {
+              if ($row_count % 2 == 0) {
+                  echo "<td class='white'></td>";
+                  $row_count++;
+              } else {
+                  echo "<td class='black'></td>";
+                  $row_count++;
+              }
+          }
+          echo "</tr>";
+      }
+    ?>
+  </table>
+</body>
+</html>
